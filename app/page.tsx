af9e8b09bbb96d2cf63e2b6d2cef44f200382940
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-10-15 19:35:11 +0300

    shadcn components added, some changes made to the shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Select } from '@/components/ui/select';
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center align-middle">
-      <div className="flex items-center justify-center align-middle mt-12 text-indigo-600 text-4xl uppercase">
-        hello from scratch
+    <>
+      <div className="flex items-center justify-center align-middle mt-12">
+        <Button>jira</Button>
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="muted">muted</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+        <Button variant="teritary">teritary</Button>
       </div>
-    </div>
+      <div className="flex flex-col items-center justify-center align-middle mt-12">
+        <Input />
+        <Select />
+      </div>
+    </>
   );
 }
