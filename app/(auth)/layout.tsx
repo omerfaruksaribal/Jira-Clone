d3c4154104e082b7cd50a9e4c5cf509dd9f9eac2
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-10-15 20:17:29 +0300

    sign in page will be implement later on

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <main className="min-h-screen bg-neutral-100">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" width={152} height={56} alt="logo" />
+          <Button variant="secondary">Sign Up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AuthLayout;
